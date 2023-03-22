bbba691c3fd93a239a8c2b21f9f901b82790ad50
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-22 10:06:31 -0600

    Refactor: #add_student test

--- spec/mod_spec.rb
+++ spec/mod_spec.rb
@@ -5,6 +5,9 @@ require './lib/student'
 RSpec.describe Mod do
   before(:each) do
     @mod_1 = Mod.new('Mod 1', 'Intro To OOP')
+    @chris = Student.new('chRIs')
+    @richard = Student.new('RiCHard')
+    @meg = Student.new('meG')
   end
 
   it 'exists' do 
@@ -15,5 +18,12 @@ RSpec.describe Mod do
     expect(@mod_1.name).to eq('Mod 1')
     expect(@mod_1.description).to eq('Intro To OOP')
     expect(@mod_1.students).to eq([])
+  end
+
+  it '#add_student' do 
+    @mod_1.add_student(@chris)
+    @mod_1.add_student(@richard)
+    @mod_1.add_student(@meg)
+    expect(@mod_1.students).to eq([@chris, @richard, @meg])
   end
 end
